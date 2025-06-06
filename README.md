4d377c7933af39de100403abb15e53c6f8a8e1bd
i-am-henri <henri.gg@icloud.com>
2025-06-06 20:42:09 +0200

    feat: published cli & created first npm package

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 **Brösel  🍞**
 
-> [!WARNING]  
-> Brösel is still in development, and not yet ready for use.
-
 Brösel is german for "crumbs", and this is describing this framework really well. It's aiming to be as tiny as possible, and fully lives inside your application. It's not installed as a dependency with npm, you just run the cli to install the neccessary files. Like shadcn/ui as framework.
 
 It's made for bun, and it's not made to be used outside of bun. You have to **use bun** to use it, you can install [bun here](https://bun.sh).
--- templates/react/src/pages/index.tsx
+++ templates/react/src/pages/index.tsx
@@ -1,69 +1,92 @@
-import { render, load } from "brosel";
-import RootLayout from "../layouts/root";
+import { load, render } from "brosel";
 import { useState } from "react";
+import RootLayout from "../layouts/root";
 
 export default load({
-    handler: async (req) => {
-        const demo = "The following text comes from the server – it's rendered on the server and sent to the client. You can include in this text files, cookies and way more."
+	handler: async (req) => {
+		const demo =
+			"The following text comes from the server – it's rendered on the server and sent to the client. You can include in this text files, cookies and way more.";
 
-        const res = await fetch("http://localhost:3000/api/demo")
+		const res = await fetch("http://localhost:3000/api/demo");
 
-        if (!res.ok) {
-            throw new Error("Failed to fetch demo text")
-        }
+		if (!res.ok) {
+			throw new Error("Failed to fetch demo text");
+		}
 
-        const text = await res.text()
+		const text = await res.text();
 
-        const defaultValue = 10
+		const defaultValue = 10;
 
-        return await render({
-            component: <Component demo={demo} text={text} defaultValue={defaultValue} />,
-            props: { demo, text }
-        })
-    }
-})
+		return await render({
+			component: (
+				<Component demo={demo} text={text} defaultValue={defaultValue} />
+			),
+			props: { demo, text },
+		});
+	},
+});
 
-export const Component = ({ demo, text, defaultValue }: { demo: string, text: string, defaultValue: number }) => {
-    const [count, setCount] = useState(defaultValue)
+export const Component = ({
+	demo,
+	text,
+	defaultValue,
+}: { demo: string; text: string; defaultValue: number }) => {
+	const [count, setCount] = useState(defaultValue);
 
-    return (
-        <RootLayout
-            metadata={{
-                title: "Brosel",
-                description: "Brosel is a modern framework for building web applications with React and Bun."
-            }}
-            props={{ demo, text, defaultValue }}
-            path="index"
-            className="flex items-center justify-center"
-        >
-            <div className="lg:w-[500px] lg:pt-24 flex flex-col space-y-12">
-                <div className="flex flex-col space-y-6">
-                    <h1 className="text-xl font-medium">
-                        Brösel
-                    </h1>
-                    <p>Brösel is a minimal fullstack framework for building web applications with React and Bun. To get started, create a new project with <code>bun create brosel</code>!</p>
-                </div>
-                <div className="flex-col flex space-y-6">
-                    <h2 className="text-lg font-medium">A little SSR demonstration</h2>
-                    <p>{demo} We can also fetch data from the server, for example:</p>
-                    <div className="flex items-center space-x-4">
-                        <code>GET /api/demo</code>
-                        <p>{text}</p>
-                    </div>
-                </div>
-                <div className="flex flex-col space-y-6">
-                    <h2 className="text-lg font-medium">Counter</h2>
-                    <p>This is a counter, that's rendered on the client. You can give it a default value from the server.</p>
-                    <div className="flex items-center justify-between">
-                        <p>Count: {count}</p>
-                        <button className="border border-neutral-200 px-2 py-0.5 rounded-md cursor-pointer" onClick={() => setCount(count + 1)}>+1</button>
-                    </div>
-                </div>
-                <a href="https://brosel.henri.is/docs" target="_blank" className="border no-underline border-neutral-200 p-4 rounded-xl flex flex-col space-y-3">
-                    <h2>Get started</h2>
-                    <p>Learn how to create your first pages with Brösel!</p>
-                </a>
-            </div>
-        </RootLayout>
-    )
-}
+	return (
+		<RootLayout
+			metadata={{
+				title: "Brosel",
+				description:
+					"Brosel is a modern framework for building web applications with React and Bun.",
+			}}
+			props={{ demo, text, defaultValue }}
+			path="index"
+			className="flex items-center justify-center"
+		>
+			<div className="lg:w-[500px] lg:pt-24 flex flex-col space-y-12">
+				<div className="flex flex-col space-y-6">
+					<h1 className="text-xl font-medium">Brösel</h1>
+					<p>
+						Brösel is a minimal fullstack framework for building web
+						applications with React and Bun. To get started, create a new
+						project with <code>bun create brosel</code>!
+					</p>
+				</div>
+				<div className="flex-col flex space-y-6">
+					<h2 className="text-lg font-medium">A little SSR demonstration</h2>
+					<p>{demo} We can also fetch data from the server, for example:</p>
+					<div className="flex items-center space-x-4">
+						<code>GET /api/demo</code>
+						<p>{text}</p>
+					</div>
+				</div>
+				<div className="flex flex-col space-y-6">
+					<h2 className="text-lg font-medium">Counter</h2>
+					<p>
+						This is a counter, that's rendered on the client. You can give it a
+						default value from the server.
+					</p>
+					<div className="flex items-center justify-between">
+						<p>Count: {count}</p>
+						<button
+							className="border border-neutral-200 px-2 py-0.5 rounded-md cursor-pointer"
+							onClick={() => setCount(count + 1)}
+						>
+							Increment Counter
+						</button>
+					</div>
+				</div>
+				<a
+					href="https://brosel.henri.is/docs"
+					target="_blank"
+					className="border no-underline border-neutral-200 p-4 rounded-xl flex flex-col space-y-3"
+					rel="noreferrer"
+				>
+					<h2>Get started</h2>
+					<p>Learn how to create your first pages with Brösel!</p>
+				</a>
+			</div>
+		</RootLayout>
+	);
+};
