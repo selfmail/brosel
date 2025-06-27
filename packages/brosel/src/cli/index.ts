2bc3d10b560137fc32729903c79e56500462bed9
i-am-henri <henri.gg@icloud.com>
2025-06-27 19:59:52 +0200

    feat(package): created own little cli library

--- packages/brosel/src/cli/index.ts
+++ packages/brosel/src/cli/index.ts
@@ -1,2 +1,34 @@
 #!/usr/bin/env bun
 
+import { createCli } from "../utils/cli";
+
+const cli = await createCli({
+	name: "Brösel CLI",
+	description: "A CLI tool for Brösel",
+	help: {
+		command: "--help",
+		answer:
+			"This is a CLI tool for Brösel. Use `brosel <command> --help` to get more information about a specific command.",
+	},
+	commands: {
+		dev: {
+			action: async (args) => {
+				console.log("Starting development server with args:", args);
+				// Here you would typically start your development server
+				// For example, you might use a bundler like Vite or Webpack
+			},
+			options: {
+				port: {
+					type: "number",
+					required: true,
+					name: "port",
+				},
+				host: {
+					type: "string",
+					required: false,
+					name: "host",
+				},
+			},
+		},
+	},
+});
